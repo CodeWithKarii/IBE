caddc621d91a30c2d63efb620a14e051ccc98a9e
CodeWithKarii <bwalton10@sycamores.indstate.edu>
2023-06-23 23:36:37 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, url_for ,send_from_directory
+from datetime import datetime 
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+   
+
+
+     if request.method == 'POST':
+        # Get the data from the POST request
+        name= request.form['name']
+        email = request.form['email']
+        # print(name)
+        # print(email)
+        return  render_template('index.html')
+     return render_template('index.html')
+
+# @app.route('/favicon.ico')
+# def fav():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
+    
+    # app.run(host='0.0.0.0', port=80)
